ac68f892e587c2ac60f00a9c285e62ae534c24b0
hyoilll <dlgydlf12345@naver.com>
2025-05-03 13:52:05 +0900

    add Icon component Edit & Delete

---
+++ components/icon/Delete.vue
@@ -0,0 +1,18 @@
+<template>
+  <Icon
+    name="mdi:trash-can-outline"
+    :class="customClass ?? 'text-red-500 cursor-pointer'"
+    :size="customSize ?? '1.3em'"
+    @click="$emit('delete')" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  customClass?: string
+  customSize?: string
+}>()
+
+defineEmits<{
+  delete: []
+}>()
+</script>
---
+++ components/icon/Edit.vue
@@ -0,0 +1,18 @@
+<template>
+  <Icon
+    name="mdi:pencil"
+    :class="customClass ?? 'text-green-500 cursor-pointer'"
+    :size="customSize ?? '1.3em'"
+    @click="$emit('edit')" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  customClass?: string
+  customSize?: string
+}>()
+
+defineEmits<{
+  edit: []
+}>()
+</script>
