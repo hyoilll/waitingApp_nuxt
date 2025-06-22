e6ce9fe5867ecbc186b9d5c4163f4c048aa1596b
hyoilll <dlgydlf12345@naver.com>
2025-06-22 17:11:54 +0900

    引数タイプ調整

--- server/utils.ts
+++ server/utils.ts
@@ -1,3 +1,5 @@
-export const getErrorMessage = (error: { message?: string; code?: string }, customMsg: string = 'sign up error') => {
-  return error.message ?? error.code ?? customMsg
+import { PostgrestError } from "@supabase/supabase-js"
+
+export const getErrorMessage = (error: PostgrestError | null, customMsg: string = 'sign up error') => {
+  return error?.message ?? error?.code ?? customMsg
 }
