9ba175ee67d680921cb4131bc7b0c7edf73b832e
hyoilll <dlgydlf12345@naver.com>
2025-07-20 16:42:11 +0900

    create button move Top

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,9 @@
 <template>
-  <footer class="bg-gray-800 text-white py-4 h-[100px] flex flex-col justify-between">
+  <footer class="bg-gray-800 text-white py-4 h-[100px] flex flex-col justify-between relative">
+    <button type="button" class="p-2 bg-blue-400 absolute right-10 -top-24 rounded-full flex items-center justify-center shadow-lg" @click="scrollToTop">
+      <Icon name="mdi:arrow-up-bold" size="3em" />
+    </button>
+
     <nav class="container mx-auto">
       <ul class="flex justify-center space-x-6">
         <li>
@@ -19,3 +23,14 @@
     </div>
   </footer>
 </template>
+
+<script setup lang="ts">
+const { scrollToTop } = useAnchorScroll({
+  toTop: {
+    scrollOptions: {
+      behavior: 'smooth',
+      offsetTop: 0,
+    }
+  }
+})
+</script>
