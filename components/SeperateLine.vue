4d94aee97c1b719d84bc87ea78dd4cf9dca96312
hyoilll <dlgydlf12345@naver.com>
2025-07-20 17:22:35 +0900

    SeperateLine component更新

--- components/SeperateLine.vue
+++ components/SeperateLine.vue
@@ -1,3 +1,9 @@
 <template>
-  <div class="border-t-2 border-gray-300 w-[80%] mx-auto my-8"></div>
+  <div :class="customClass ? customClass : 'border-t-2 border-gray-300 w-[80%] mx-auto my-8'"></div>
 </template>
+
+<script setup lang="ts">
+const { customClass } = defineProps<{
+  customClass?: string
+}>()
+</script>
