c58fbe8a9180322d9558e80c719a8f6e7c144fc3
hyoilll <dlgydlf12345@naver.com>
2025-08-09 16:38:39 +0900

    呼び出し押さずに、入店できないようにする

--- server/api/admin/markentered.ts
+++ server/api/admin/markentered.ts
@@ -1,11 +1,28 @@
 import { serverSupabaseClient } from '#supabase/server'
 import { getErrorMessage } from '../../utils'
 
+interface EntryInfo {
+  is_called: boolean;
+}
+
 export default eventHandler(async (event) => {
   const query = await readBody(event)
   const client = await serverSupabaseClient(event)
 
   try {
+    const {data, error: selectError} = await client
+      .from('entries')
+      .select('is_called')
+      .eq('id', query.entryId);
+
+    if (selectError) {
+      return { error: getErrorMessage(selectError, '入店情報の取得に失敗しました') };
+    }
+
+    if (data !== null && (data as EntryInfo[]).at(0)?.is_called === false) {
+      return { error: '先に呼出を実行してください。' };
+    }
+    
     const { error } = await client
       .from('entries')
       .update({ status: 'entered', entered_at: new Date() })
